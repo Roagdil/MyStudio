5a34fad2d4df0b47fd71b8a6495fdeaee2951779
Roagdilz <rodrigoagustindiazlopez@gmail.com>
2024-08-24 23:50:56 -0300

    hola

--- Algoritmos y programacion/TP/Teoria_3/Teoria_3/Ejercicio_2/Program.cs
+++ Algoritmos y programacion/TP/Teoria_3/Teoria_3/Ejercicio_2/Program.cs
@@ -26,22 +26,31 @@ namespace Ejercicio_2
 				menu();
 				respuesta = Console.ReadLine();
 				switch (respuesta) {
-					case "a": break;
-					case "b": break;
-					case "c": break;
+					case "a": // inscribir un alumno
+						nuevoAlumno(ref alumno);
+						listaAlumno.Add(alumno);
+						
+						break;
+					case "b":
+						eliminarAlumno(ref listaAlumno);
+						break;
+					case "c":
+						Console.WriteLine("Se encuentran {0} alumnos inscriptos",length(listaAlumno));
+						break;
 					case "d": break;
 					case "e": break;
-					case "" : Console.WriteLine("Adios"); break;
+					case "" : break;
 					default:
 						Console.Clear();
 						Console.WriteLine("Respuesta incorrecta");
 						break;
 				}
 				
+				
 			}while (respuesta != "");
 			// TODO: Implement Functionality Here
 			
-			Console.Write("Press any key to continue . . . ");
+			Console.Write("Adios");
 			Console.ReadKey(true);
 		}
 		
@@ -58,5 +67,41 @@ namespace Ejercicio_2
 			
 			
 		}
+		public static void nuevoAlumno(ref ArrayList alumno){
+			string nombre,apellido,escuela;
+			long dni;
+			Console.WriteLine("Ingrese nombre del alumno");
+			nombre = Console.ReadLine();
+			Console.WriteLine("Ingrese apellido del alumno");
+			apellido = Console.ReadLine();
+			Console.WriteLine("Ingrese dni del alumno");
+			dni = long.Parse( Console.ReadLine());
+			Console.WriteLine("Ingrese Escuela del alumno");
+			escuela =  Console.ReadLine();
+			alumno.Add(nombre);
+			alumno.Add(apellido);
+			alumno.Add(dni);
+			alumno.Add(escuela);
+			
+		}
+		public static void eliminarAlumno(ref ArrayList lista){
+			ArrayList copia= new ArrayList();
+			Console.WriteLine("Ingrese el dni del alumno");
+			long dni = long.Parse(Console.ReadLine());
+			foreach (ArrayList element in lista) {
+				if ((long)element[2] != dni){
+					copia.Add(element);
+				}
+			}
+			lista = copia;
+		}
+		public static int length(ArrayList lista){
+			int i=0;
+			foreach (var element in lista) {
+				i++;
+			}
+			return i;
+		}
+		
 	}
 }
