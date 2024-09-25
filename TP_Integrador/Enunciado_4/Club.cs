060550a1d7e5e14959ec4a5f0fc4c29d771bb385
Roagdilz <rodrigoagustindiazlopez@gmail.com>
2024-09-25 19:14:30 -0300

    2.3

--- TP_Integrador/Enunciado_4/Club.cs
+++ TP_Integrador/Enunciado_4/Club.cs
@@ -14,7 +14,7 @@ namespace Enunciado_4
 	/// Description of Club.
 	/// </summary>
 	public class Club
-	{	
+	{
 		Atleta [] atletas;
 		Deporte [] deportes;
 		public Club()
@@ -27,6 +27,60 @@ namespace Enunciado_4
 					Console.WriteLine(atleta);
 				}
 			}
+		}
+		public void AgregarDeporte(Deporte deporte){
+			int cap = this.deportes.Length;
+			Deporte [] deportes= new Deporte[cap+1];
+			for (int i = 0; i < cap; i++) {
+				deportes[i]= this.deportes[i];
+			}
+			deportes[-1] = deporte;
+			this.deportes = deportes;
+			
+		}
+		public void AgregarAtleta(Atleta atleta){
+			int cap = this.atletas.Length;
+			Atleta [] atletas= new Atleta[cap+1];
+			for (int i = 0; i < cap; i++) {
+				atletas[i]= this.atletas[i];
+			}
+			atletas[-1] = atleta;
+			this.atletas = atletas;
+		}
+		public void EliminarAtleta(Atleta atleta){
+			int cap = this.atletas.Length;
+			int cont= 0;
+			Atleta [] atletas= new Atleta[cap-1];
+			try {
+				for (int i = 0; i < cap; i++) {
+					if(atleta != this.atletas[i]){
+						atletas[i]= this.atletas[i];
+					}
+				}
+				this.atletas = atletas;
+			} catch (IndexOutOfRangeException) {
+				
+				Console.WriteLine("Cliente ingresado no existente");
+			}
+			
+		}
+		public void EliminarDeporte(Deporte deporte){
+			int cap = this.deportes.Length;
+			int cont= 0;
+			Deporte [] deportes= new Deporte[cap-1];
+			try {
+				for (int i = 0; i < cap; i++) {
+					if(deporte != this.deportes[i]){
+						deportes[i]= this.deportes[i];
+					}
+					
+				}
+				this.deportes = deportes;
+			} catch (IndexOutOfRangeException) {
+				
+				Console.WriteLine("Deporte ingresado no existente");
+			}
+			
 		}
 	}
 }
